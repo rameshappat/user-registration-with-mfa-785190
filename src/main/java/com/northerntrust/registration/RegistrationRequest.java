bde43f8a6d3e83b84082b503c6fbb3951fd57403
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 16:19:51 -0700

    feat: add RegistrationRequest.java

---
+++ src/main/java/com/northerntrust/registration/RegistrationRequest.java
@@ -0,0 +1,43 @@
+package com.northerntrust.registration;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class RegistrationRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @NotBlank
+    private String phoneNumber;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
